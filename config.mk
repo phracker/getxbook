46e8d5f0690f4d8c4184e47d6f62fb3f409901b9
Nick White <hg@njw.me.uk>
2011-07-17 16:22:30 +0100

    Separate glibc & musl options in config.mk

--- config.mk
+++ config.mk
@@ -4,10 +4,14 @@ VERSION = prealpha
 # paths
 PREFIX = /usr/local
 
-# flags
 CFLAGS = -ansi -pedantic -Wall -Wextra -Werror -g -D_POSIX_C_SOURCE=200112L -DVERSION=\"$(VERSION)\"
+
+# musl static
+CC = musl-gcc
 LDFLAGS = -static #-s
 
-# compiler and linker
-CC = musl-gcc
+# glibc dynamic
+#CC = cc
+#LDFLAGS = 
+
 LD = ld
