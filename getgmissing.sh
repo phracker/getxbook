6b8f282b5b0c0621ebcf6124f6a6cb90fed47297
Nick White <hg@njw.me.uk>
2011-08-02 21:05:19 +0100

    More getgmissing updates

--- getgmissing.sh
+++ getgmissing.sh
@@ -7,6 +7,6 @@ getgbook -p $1 2>/dev/null | while read i
 do
 	code=`echo $i|awk '{print $1}'`
 	num=`echo $i|awk '{print $2}'`
-	test -n "$num" && num=`printf '%04d' || num=$code
+	test -n "$num" && num=`printf '%04d' $num` || num=$code
 	test -f $num.png || echo $code | getgbook $1
 done
